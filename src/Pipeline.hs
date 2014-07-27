ff07acf9aaf05af4bf8ee7449760f6606c00b117
Zuzia <katarzyna.streich@gmail.com>
2014-07-27 18:17:33 +0200

    adjust pipeline module to previous changes

--- src/Pipeline.hs
+++ src/Pipeline.hs
@@ -3,10 +3,13 @@ module Pipeline
 
 import Protocol
 import Reader
+import Task as T
 import qualified Worker as W
 import qualified Data.List as DL
 import System.Directory (getCurrentDirectory)
-import Control.Monad (liftM)
+import Control.Monad
+import Control.Monad.Trans (lift)
+import Control.Monad.Trans.Maybe
 import Control.Exception (handle)
 
 --TODO functions in class Stage;
@@ -26,7 +29,7 @@ data Grouping
     | Group_all
 
 data Stage = Stage {
-    process_fun :: W.Process,
+    process_fun :: T.Process,
     name :: String,
     grouping :: Grouping
 }
@@ -42,37 +45,40 @@ stage_names :: Pipeline -> [String]
 stage_names pipeline =
     map name pipeline
 
-get_process_fun :: Pipeline -> String -> Maybe W.Process
+get_process_fun :: Pipeline -> String -> Maybe T.Process
 get_process_fun pipeline stage_name =
     DL.lookup stage_name assoc
     where assoc = map (\p -> (name p, process_fun p)) pipeline
 
-run_task :: FilePath -> String -> Task -> [Input] -> W.Process -> IO (Maybe Master_msg)
+run_task :: FilePath -> String -> Task -> [Input] -> T.Process -> MaybeT IO Master_msg
 run_task pwd file_templ task list_inputs process_fun = do
-    outputs <- W.run_stage pwd file_templ task list_inputs process_fun
+    -- TODO call init function
+    -- TODO call done function
+    outputs <- lift $ W.run_stage pwd file_templ task list_inputs process_fun --check combine, sort flags + input_hook fun
     mapM W.send_outputs outputs
     exchange_msg W_done
     
 --analogous to classic run
-run :: Pipeline -> IO ()
+run :: Pipeline -> MaybeT IO ()
 run pipeline = do
-    send_worker
-    Just M_ok <- recive
-    Just (M_task task) <- exchange_msg W_task
-    pwd <- getCurrentDirectory
+    pwd <- lift getCurrentDirectory
+    lift send_worker
+    W.expect_ok
+    M_task task <- exchange_msg W_task
     let task_stage = stage task
     let file_templ = task_stage ++ "_out_"
-    --Just (M_task_input t_input) <- exchange_msg $ W_input Empty
     inputs_list <-  W.get_inputs []
     case (get_process_fun pipeline task_stage) of
         Just process_fun -> do run_task pwd file_templ task inputs_list process_fun
         Nothing -> do exchange_msg $ W_fatal "Non-existent stage"
-    --Just M_ok <- exchange_msg W_done
     return ()
 
 --for example pipeline = [("map", Split), ("shuffle", Group_node), ("reduce", Group_all)]
 start_pipeline :: Pipeline -> IO () 
-start_pipeline pipeline =
+start_pipeline pipeline = do
     --handle error_wrapper (run pipeline) --TODO error wrapper
-    run pipeline
+    result <- runMaybeT $ run pipeline
+    case result of
+        Nothing -> (runMaybeT $ exchange_msg $ W_fatal "Protocol error") >> return ()
+        Just _ -> return ()
 
